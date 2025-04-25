b33c5b714345bf6475972c69d737ba3ec4fcb7f0
Jawadtp <mjawadtp@salesforce.com>
2025-04-25 12:32:21 +0530

    Add github issues fetcher tool to common tools

--- src/tools/common_tools.py
+++ src/tools/common_tools.py
@@ -1,4 +1,7 @@
 import os
+import requests
+import json
+from typing import Optional
 
 from langchain.pydantic_v1 import BaseModel, Field
 from langchain.tools import BaseTool, StructuredTool, tool
@@ -22,9 +25,40 @@ def weather(city: str) -> str:
     else:
         return f"Sorry, I don't have weather information for {city}."
 
+
+class GithubIssuesToolInput(BaseModel):
+    repo: str = Field(description="Name of the GitHub repository to fetch issues from", examples=["IPL-Score-Tracker", "Solution Cloud"])
+    state: Optional[str] = Field(description="State of the issues to fetch. That is, open or closed", examples=["open", "closed"])
+    
+    
+@tool("github-tool", args_schema=GithubIssuesToolInput, return_direct=False)
+def fetch_github_issues(repo: str):
+    """Fetches issues of specified state (open or closed) from the specified GitHub repository"""
+    temp = json.loads(repo)
+    repo_name = temp.get("repo")
+    state = temp.get("state")
+    GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+    url = f"https://api.github.com/repos/mjawadtp/{repo_name}/issues"
+    headers = {
+        "Authorization": f"token {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    params = {
+        "state": state  # 'open', 'closed', or 'all'
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch issues: {response.status_code} - {response.text}")
+
+
 # --- Add all tools here ---
 COMMON_TOOLS = [
     weather,
+    fetch_github_issues
 ]
 
 # --- Example of how to use the tools ---
@@ -33,4 +67,4 @@ if __name__ == "__main__":
     prompt = hub.pull("hwchase17/react")
     agent = create_react_agent(llm, COMMON_TOOLS, prompt)
     agent_executor = AgentExecutor(agent=agent, tools=COMMON_TOOLS, verbose=True)
-    agent_executor.invoke({"input": "what is the weather in Los Angeles?"})
+    agent_executor.invoke({"input": "Can you fetch open issues from the repo IPL-Score-Tracker and list them along with their description"})
